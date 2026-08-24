8eb417a496ab65b4b21245c6c76295ff52970d01
Avani-bansal <AVANI.32144@stu.upes.ac.in>
2026-08-24 14:52:35 +0530

    Add files via upload

---
+++ q29.c
@@ -0,0 +1,13 @@
+//Q29: Write a program to calculate the factorial of a number.
+#include <stdio.h>
+int main()
+{
+    int n, i, fact = 1;
+    scanf("%d", &n);
+    for(i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+    printf("%d", fact);
+    return 0;
+}
