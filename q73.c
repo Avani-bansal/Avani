82e72ef28b1e86b97bd0fa91624920b3d4b820e4
Avani-bansal <AVANI.32144@stu.upes.ac.in>
2026-09-15 15:21:36 +0530

    Add files via upload

---
+++ q73.c
@@ -0,0 +1,23 @@
+//Q73: Find the sum of each row of a matrix and store it in an array.
+#include <stdio.h>
+int main()
+{
+    int a[10][10], sum[10];
+    int rows, columns, i, j;
+    scanf("%d %d", &rows, &columns);
+    for(i = 0; i < rows; i++)
+    {
+        sum[i] = 0;
+
+        for(j = 0; j < columns; j++)
+        {
+            scanf("%d", &a[i][j]);
+            sum[i] = sum[i] + a[i][j];
+        }
+    }
+    for(i = 0; i < rows; i++)
+    {
+        printf("%d ", sum[i]);
+    }
+    return 0;
+}
