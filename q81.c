59d21fdc99d7677e494150756ea68927ccc2c1b0
Avani-bansal <AVANI.32144@stu.upes.ac.in>
2026-09-19 20:28:44 +0530

    Add files via upload

---
+++ q81.c
@@ -0,0 +1,15 @@
+//Q81: Count characters in a string without using built-in length functions.
+#include <stdio.h>
+int main()
+{
+    char str[100];
+    int count = 0, i = 0;
+    scanf("%[^\n]", str);
+    while(str[i] != '\0')
+    {
+        count++;
+        i++;
+    }
+    printf("%d", count);
+    return 0;
+}
