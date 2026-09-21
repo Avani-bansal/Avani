9c7de157d04bef05c9b72bc717d201130dd113f8
Avani-bansal <AVANI.32144@stu.upes.ac.in>
2026-09-21 22:43:25 +0530

    Add files via upload

---
+++ q86.c
@@ -0,0 +1,25 @@
+//Q86: Check if a string is a palindrome.
+#include <stdio.h>
+int main()
+{
+    char str[100];
+    int i, length = 0, palindrome = 1;
+    scanf("%[^\n]", str);
+    while(str[length] != '\0')
+    {
+        length++;
+    }
+    for(i = 0; i < length / 2; i++)
+    {
+        if(str[i] != str[length - 1 - i])
+        {
+            palindrome = 0;
+            break;
+        }
+    }
+    if(palindrome == 1)
+        printf("Palindrome");
+    else
+        printf("Not palindrome");
+    return 0;
+}
