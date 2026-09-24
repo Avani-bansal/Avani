399aedfc05a68a667319fbae8ed36e09d1bb8209
Avani-bansal <AVANI.32144@stu.upes.ac.in>
2026-09-24 13:31:47 +0530

    Add files via upload

---
+++ q92.c
@@ -0,0 +1,21 @@
+//Q92: Find the first repeating lowercase alphabet in a string.
+#include <stdio.h>
+int main()
+{
+    char str[100];
+    int count[26] = {0};
+    int i = 0;
+    scanf("%s", str);
+    while(str[i] != '\0')
+    {
+        count[str[i] - 'a']++;
+
+        if(count[str[i] - 'a'] == 2)
+        {
+            printf("%c", str[i]);
+            return 0;
+        }
+        i++;
+    }
+    return 0;
+}
